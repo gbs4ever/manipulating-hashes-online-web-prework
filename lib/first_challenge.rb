dcd4759ef827b52a87d235d74368f64105eb3b7e
Sim Greenbaum <gbs4ever2@gmail.com>
2018-12-27 03:02:32 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,13 +15,13 @@ def first_challenge
 
   contacts.each do |person, data|
   #at this level, "person" is Jon Snow or Freddy and "data" is a hash of key/value pairs
-  #to iterate over the "data" hash, we can use the following line: 
+  #to iterate over the "data" hash, we can use the following line:
    data.each do |attribute, value|
     # "attribute" describes the key of :name, :email, :favorite_icecream_flavors, or :knows
   if attribute == :favorite_icecream_flavors
       value.each do |flavor|
     flavor.shift
-        
+
       end
     end
   end
